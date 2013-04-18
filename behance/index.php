dc62c601b2e208368b26968224432b748a84b8d0
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2013-04-18 10:34:24 +0200

    minor updates

--- behance/index.php
+++ behance/index.php
@@ -62,7 +62,6 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
         <script src="../js/plugins.js"></script>
-        <script src="../js/behance.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
